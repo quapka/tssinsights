ee9fff9addabe6675d89aec835f731a4410d68f8
mseckar <seckar.martin@gmail.com>
2024-07-24 14:06:02 +0000

    Refactor, added more sane bool rules

--- mpcinsights/poc.py
+++ mpcinsights/poc.py
@@ -1,6 +1,6 @@
 class MPCanalyzer():
 
-    def __init__(self, limit: int = 10):
+    def __init__(self, limit: int = 6):
         self.limit = limit
         self.policies = self.bruteforce_policies(self.limit)
     
@@ -11,24 +11,28 @@ class MPCanalyzer():
         out = []
 
         for n in range(1, limit):
-            for k in range(1, limit):
+            for t in range(1, limit):
                 for e in range(1, limit):
-                    if k > n or e > n:
+                    if t > n or e > n:
                         continue
-                    out.append((n, k, e))
+                    out.append((t, n, e))
         return out
 
-    def find_commonalities(self, policy: tuple[int, int, int]) -> None:
-        common_policies = [policy_candidate for policy_candidate in self.policies if self.has_same_properties(policy_candidate, policy)]
-        print(f"Policy {policy} has common properties with {len(common_policies)} policies in limit {self.limit}: {common_policies}")
+    def analyze(self, policy: tuple[int, int, int]) -> None:
+        common_policies = [policy_candidate for policy_candidate in self.policies if self.has_common_properties(policy_candidate, policy)]
+        print(f"Policy {policy} has common properties with {len(common_policies)} ({(len(common_policies)/len(self.policies)* 100):.2f}%) policies in limit {self.limit}: {common_policies}")
 
-    def has_same_properties(self, candidate: tuple[int, int, int], policy: tuple[int, int, int]) -> bool:
-        return (candidate[0] % policy[0] == 0 and candidate[1] % policy[1] == 0 and candidate[1] % policy[1] == 0)
+    def has_common_properties(self, candidate: tuple[int, int, int], base: tuple[int, int, int]) -> bool:
 
+        same_roles = (candidate[2] == base[2])
+        multiples = (candidate[0] % base[0] == 0 and candidate[1] % base[1] == 0)
+        same_t_e = ((candidate[0] == candidate[2]) if base[0] == base[2] else False)
+        larger_n = ((candidate[0] < candidate[1]) if base[0] < base[1] else False)
+        return (same_roles and multiples) or (same_t_e and larger_n)
 
 if __name__=="__main__":
-
-    policy = (3,2,2)
-    mpcanalyzer = MPCanalyzer(10)
+    # t out of n with e distinct roles
+    policy = (2,3,2)
+    mpcanalyzer = MPCanalyzer()
     print(mpcanalyzer)
-    mpcanalyzer.find_commonalities(policy)
+    mpcanalyzer.analyze(policy)
