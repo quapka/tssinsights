3cc330f1b129ed8b4a7710335489f2c90834a89e
mseckar <seckar.martin@gmail.com>
2024-09-09 12:59:18 +0000

    Added stronger typing and commonality reasoning
    Remove cache
    
    added gitignore rule for cache

--- tssinsights/poc.py
+++ tssinsights/poc.py
@@ -1,13 +1,18 @@
+from properties import *
+from policyproperty import Policy
+
+
 class MPCanalyzer():
 
     def __init__(self, limit: int = 6):
         self.limit = limit
         self.policies = self.bruteforce_policies(self.limit)
+        self.properties = [SameRolesProp, MultipleProp, SameTEProp, LargerNProp, SmallTProp, SameTNProp]
     
     def __str__(self):
         return f"{self.policies}"
 
-    def bruteforce_policies(self, limit: int) -> list[tuple[int, int, int]]:
+    def bruteforce_policies(self, limit: int) -> Policy:
         out = []
 
         for n in range(1, limit):
@@ -15,25 +20,38 @@ class MPCanalyzer():
                 for e in range(1, limit):
                     if t > n or e > n:
                         continue
-                    out.append((t, n, e))
+                    out.append([(t, n, e), []])
         return out
 
-    def analyze(self, policy: tuple[int, int, int]) -> None:
+    def analyze(self, policy: Policy) -> None:
+        policy = [policy, []]
         common_policies = [policy_candidate for policy_candidate in self.policies if self.has_common_properties(policy_candidate, policy)]
-        print(f"Policy {policy} has common properties with {len(common_policies)} ({(len(common_policies)/len(self.policies)* 100):.2f}%) policies in limit {self.limit}: {common_policies}")
+        print(f"Policy {policy[0]} has common properties with {len(common_policies)} ({(len(common_policies)/len(self.policies)* 100):.2f}%) policies in limit {self.limit}:")
 
-    def has_common_properties(self, candidate: tuple[int, int, int], base: tuple[int, int, int]) -> bool:
+        for policy in common_policies:
+            print(f"Policy {policy[0]}: ")
+            for prop in policy[1]:
+                print(prop.description())
 
-        same_roles = (candidate[2] == base[2])
-        multiples = (candidate[0] % base[0] == 0 and candidate[1] % base[1] == 0)
-        same_t_e = ((candidate[0] == candidate[2]) if base[0] == base[2] else False)
-        larger_n = ((candidate[0] < candidate[1]) if base[0] < base[1] else False)
-        small_t = (candidate[0] == 1 if base[0] == 1 else candidate[0] != 1)
-        return (same_roles and multiples) or (same_t_e and larger_n and small_t) 
+    def has_common_properties(self, candidate: Policy, base: Policy) -> bool:
+
+        #placeholder pre higher-level rules
+        final_sum = []
+
+        for prop in self.properties:
+            if prop.check_property(candidate, base):
+                final_sum.append(True)
+                candidate[1].append(prop)
+            else:
+                final_sum.append(False)
+        
+        return (final_sum[0] and final_sum[1]) or (final_sum[2] and final_sum[3]) or (final_sum[4]) or (final_sum[5] and final_sum[0])
+
+
 
 if __name__=="__main__":
     # t out of n with e distinct roles
     policy = (2,3,2)
     mpcanalyzer = MPCanalyzer()
-    print(mpcanalyzer)
+    #print(mpcanalyzer)
     mpcanalyzer.analyze(policy)
